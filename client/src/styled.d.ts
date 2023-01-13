467fd89a1fa08f5e7dea90a68023b76a05a6cf8a
leejanghe <babybye777@gmail.com>
2023-01-13 21:57:48 +0900

    환경 셋팅

---
+++ client/src/styled.d.ts
@@ -0,0 +1,14 @@
+// declare module "styled-components" {
+//   export interface DefaultTheme {
+//     red: string;
+//     black: {
+//       veryDark: string;
+//       darker: string;
+//       lighter: string;
+//     };
+//     white: {
+//       darker: string;
+//       lighter: string;
+//     };
+//   }
+// }

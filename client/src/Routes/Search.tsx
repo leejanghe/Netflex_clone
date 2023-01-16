befcf49bcd8ab4dc6714ae0a3a38102f4132e43e
leejanghe <babybye777@gmail.com>
2023-01-16 22:50:15 +0900

    done

--- client/src/Routes/Search.tsx
+++ client/src/Routes/Search.tsx
@@ -1,6 +1,9 @@
 import React from "react";
-
+import { useLocation } from "react-router";
 function Search() {
+  const location = useLocation();
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
   return <div>afds</div>;
 }
 
